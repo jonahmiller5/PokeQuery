4cb77126ba1f27361dd6d38877a39179b9e4cca3
Jonah <jonahmil@seas.upenn.edu>
2021-04-26 18:08:45 -0400

    first commit

---
+++ src/main/java/nets150hw5/businesslogic/PokeAPICaller.java
@@ -0,0 +1,8 @@
+package nets150hw5.businesslogic;
+
+public class PokeAPICaller {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/nets150hw5/datamodel/PokeAPILocation.java
@@ -0,0 +1,24 @@
+package nets150hw5.datamodel;
+
+public class PokeAPILocation {
+    private String name;
+    private String url;
+
+    /**
+     * Constructor.
+     * @param name  The location name
+     * @param url   The location's PokeAPI url
+     */
+    public PokeAPILocation(final String name, final String url) {
+        // TODO: implement
+    }
+
+    /**
+     * A static method to create a PokeAPILocation object from JSON.
+     * @return parsed PokeAPILocation instance
+     */
+    public static PokeAPILocation fromJson() {
+        // TODO: implement
+        return null;
+    }
+}
